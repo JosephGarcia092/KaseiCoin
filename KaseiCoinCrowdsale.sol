07586e72ce860ee48f4e8cd693dcefc30ad3547d
JosephGarcia092 <98070765+JosephGarcia092@users.noreply.github.com>
2022-08-03 17:05:42 -0700

    finished main
    Completed the main section..
    looking at completing the optional part

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -3,19 +3,30 @@ pragma solidity ^0.5.5;
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
         uint256 rate,
         address payable wallet,
-        KaseiCoin token // KaseiCoin is an imported function from the KaseiCoin.sol file
-    ) public Crowdsale(rate, wallet, token) {
+        KaseiCoin token, // KaseiCoin is an imported function from the KaseiCoin.sol file
+        uint goal,// the goal of the crowdsale
+        uint open,// the open of the crowdsale
+        uint close// the close of the crowdsale
+    ) public 
+    Crowdsale(rate, wallet, token)
+     CappedCrowdsale(goal)
+     TimedCrowdsale(open, close)
+     RefundableCrowdsale(goal)
+     {
         // constructor can stay empty
     }
 }
@@ -40,15 +51,17 @@ contract KaseiCoinCrowdsaleDeployer {
         kasei_token_address = address(token);
 
         // Create a new instance of the `KaseiCoinCrowdsale` contract
-        //KaseiCoinCrowdsale
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale (1, wallet, token, goal, now, now + 24 weeks);
+
+        //KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale (1, wallet, token);
             
         // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
-        // YOUR CODE HERE!
+        kasei_crowdsale_address = address(kasei_crowdsale);
 
         // Set the `KaseiCoinCrowdsale` contract as a minter
-        // YOUR CODE HERE!
+        token.addMinter(kasei_crowdsale_address);
         
         // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
-        // YOUR CODE HERE!
+        token.renounceMinter();
     }
 }
